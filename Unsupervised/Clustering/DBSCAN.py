ed8f6d45695106b0d2705f2022594602f21925f4
Jie Hu <jieuhyL@gmail.com>
2020-04-13 00:23:00 -0700

    Update DBSCAN.py

--- Unsupervised/Clustering/DBSCAN.py
+++ Unsupervised/Clustering/DBSCAN.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 from sklearn.cluster import DBSCAN
+from collections import Counter
 
 # define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
@@ -27,4 +28,52 @@ for cluster in clusters:
 	# create scatter of these samples
 	plt.scatter(X[row_ix, 0], X[row_ix, 1])
 # show the plot
+plt.show()
+
+
+#====
+
+# Starting a tally of total iterations
+dbscan_cluster  = []
+count_cluster   = []
+
+eps_space = np.arange(0.1, 5, 0.1)
+min_samples_space = np.arange(1, 20, 1)
+min_clust = 2
+max_clust = 4
+
+# Looping over each combination of hyperparameters
+for eps_val in eps_space:
+    for samples_val in min_samples_space:
+
+        dbscan_grid = DBSCAN(eps = eps_val,
+                             min_samples = samples_val)
+
+        clusters = dbscan_grid.fit_predict(X)
+        cluster_count = Counter(clusters)
+        n_clusters = len(np.unique(clusters))
+
+        if n_clusters >= min_clust and n_clusters <= max_clust:
+
+            dbscan_cluster.append([eps_val,
+                                   samples_val,
+                                   n_clusters])
+
+            count_cluster.append(cluster_count)
+
+
+# last fit
+model = DBSCAN(eps=0.1, min_samples=18)
+# fit model and predict clusters
+yhat = model.fit_predict(X)
+# retrieve unique clusters
+clusters = np.unique(yhat)
+# create scatter plot for samples from each cluster
+for cluster in clusters:
+	# get row indexes for samples with this cluster
+	row_ix = np.where(yhat == cluster)
+	# create scatter of these samples
+	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+# show the plot
+plt.legend(clusters)
 plt.show()
