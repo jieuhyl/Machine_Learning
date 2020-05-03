64cd3bd2c76c4455ef2f8fa2043f7839cefd5f2e
Jie Hu <jieuhyL@gmail.com>
2020-05-03 03:16:59 -0700

    Update hgb_u1.py

--- Supervised/Classification/hgb_u1.py
+++ Supervised/Classification/hgb_u1.py
@@ -6,7 +6,7 @@ Created on Sun May  3 03:14:13 2020
 """
 
 
-''' 8: HistGradient Boosting'''
+''' 9: HistGradient Boosting'''
 from sklearn.experimental import enable_hist_gradient_boosting 
 from sklearn.ensemble import HistGradientBoostingClassifier
 clf_hgb = HistGradientBoostingClassifier(validation_fraction=0.2,
