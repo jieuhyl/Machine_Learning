a8d417fd96bc6b708b6705156b94e0d9babb8eac
Jie Hu <jieuhyL@gmail.com>
2020-05-03 04:44:36 -0700

    Update dt_v1.py

--- Supervised/Regression/dt_v1.py
+++ Supervised/Regression/dt_v1.py
@@ -72,7 +72,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
 
-# svm =========================================================================
+# dt =========================================================================
 mod_dt = DecisionTreeRegressor(random_state= 1337)
 mod_dt.fit(X_train, y_train)
 
